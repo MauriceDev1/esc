95257f3daa79a7ed4259fff1ac8c34e18d086acd
MauriceDev1 <mrvolkwyn@gmail.com>
2024-12-17 12:46:51 +0000

    fixed area id page try 2

--- app/(root)/book/area/[id]/page.tsx
+++ app/(root)/book/area/[id]/page.tsx
@@ -2,12 +2,18 @@
 
 import React, { useEffect, useState } from "react";
 import Locations from "@/data/locations";
-import { useRouter } from "next/navigation"; // Correct import for Next.js 13+
+import { useRouter } from "next/navigation";
 
 interface AreaProps {
   params: {
     id: string;
   };
+}
+
+export async function generateStaticParams() {
+  // If using static generation, you should provide a list of all possible IDs
+  const ids = Locations.map((location) => ({ id: location.id.toString() }));
+  return ids;
 }
 
 export default function Area({ params }: AreaProps) {
@@ -23,29 +29,23 @@ export default function Area({ params }: AreaProps) {
 
   const handleSelect = (locationName: string) => {
     if (typeof window !== "undefined") {
-      // Get existing selectedCity data
       const existingData = localStorage.getItem("selectedCity");
       let updatedData;
 
       if (existingData) {
-        // Parse and append area to existing data
         const parsedData = JSON.parse(existingData);
         updatedData = { ...parsedData, area: locationName };
       } else {
-        // If no existing data, create new
         updatedData = { area: locationName };
       }
 
-      // Store the updated data in localStorage
       localStorage.setItem("selectedCity", JSON.stringify(updatedData));
-
-      // Redirect to the booking page
       router.push(`/book/gender`);
     }
   };
 
   if (!area) {
-    return <div>Loading...</div>; // Add a fallback UI for better UX
+    return <div>Loading...</div>;
   }
 
   const maxlen = area.location?.length;
