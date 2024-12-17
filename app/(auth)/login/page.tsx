3325bc7ba89ec52526b6cc1675133a02b4500419
MauriceDev1 <mrvolkwyn@gmail.com>
2024-12-17 21:00:58 +0000

    fixed mobile screen

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -22,7 +22,7 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center w-full fixed top-0 left-0 bg-black px-2">
+    <div className="min-h-screen flex justify-center items-center w-full fixed top-0 left-0 bg-black px-2 md:px-0">
       <Card className="w-full max-w-md shadow-lg">
         <CardHeader>
           <CardTitle className="text-center">Login</CardTitle>
