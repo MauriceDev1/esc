f905cea87130bc0e8fea5cddac102ac51229a731
MauriceDev1 <mrvolkwyn@gmail.com>
2024-12-17 18:15:07 +0000

    fix the book now button text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className='w-full flex h-96 bg-custom-image'>
         <Link href="/book/city" className='m-auto'>
             <Button className='rounded-sm text-md px-10 py-3 h-12 rounded-full'>
-                Book Now
+                Book An Escort
             </Button>
         </Link>
     </div>
